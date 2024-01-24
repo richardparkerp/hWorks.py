a060bfa8bceecf5ee7fb9ad632c05dc81e66853b
richardparkerp <isabayev.b.03@gmail.com>
2024-01-24 18:51:31 +0600

    start

--- main.py
+++ main.py
@@ -54,3 +54,36 @@ def checkLucky(str):
         print("Simple!")
 
 checkLucky("123123")
+
+n = int(input())
+partial_factorial = 1
+partial_sum = 0
+for i in range(1, n + 1):
+    partial_factorial *= i
+    partial_sum += partial_factorial
+print(partial_sum)
+
+
+num=int(input("Enter num :"))
+s=""
+for i in range(1 ,num+1):
+    s+=str(i)
+    print(s)
+
+
+n = int(input())
+sum = 0
+for i in range(1, n + 1):
+    sum += i
+for i in range(n - 1):
+    sum -= int(input())
+print(sum)
+
+
+num=int(input("Etner num :"))
+
+for i in range(1,num+1):
+    if i**2<=num:
+        print(i**2,end=" ")
+    else:
+        break
