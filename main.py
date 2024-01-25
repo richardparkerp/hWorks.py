7d7540d30c908759b893beba7448204ff889184e
richardparkerp <isabayev.b.03@gmail.com>
2024-01-25 13:05:54 +0600

    start

--- main.py
+++ main.py
@@ -1,89 +1,59 @@
-# Напишите программу, где я ввожу логин и пароль. И если данные были введены верно,
-# то мы выводим Authentication completed, иначе Invalid login or password. Правильность логина не зависит от
-# (Логин должен быть user, пароль - qwerty)
-login = input("Enter login : ")
-password = input("Enter password : ")
+import itertools
 
-if (login == "user" and password == "qwerty"):
-    print("Authentication completed!")
-else:
-    print("Invalid login or password!")
 
-# Напишите программу обмена валют, где я ввожу сумму в тенге
-# и выбираю на какую валюту хочу перевести. (Курс USD – 420, EUR – 510, RUB - 5.8).
+# Задание 5 (dropwhile): Напишите функцию drop_less_than, которая принимает список
+# целых чисел и целое число n в качестве входных данных и возвращает список целых
+# чисел из входного списка, начиная с первого числа, которое не меньше n.
+# Например, drop_less_than([1, 2, 3, 4, 5], 3) должна вернуть [3, 4, 5].
 
-sum=-1
-while(sum<=0):
-    sum = int(input("Enter your sum im KZT: "))
-    print("CHOISE CONVERT VALUTE\n"
-          "1 - USD\n"
-          "2 - EUR\n"
-          "3 - RUB\n"
-          "0 - Exit")
-    choise=input("YOUR CHOISE : ")
-    if choise=="1":
-        answer = sum/420
-        print("Anwer %.2f" % answer + "USD")
-    elif choise=="2":
-        answer = sum / 510
-        print("Anwer %.2f" % answer + "EUR")
-    elif choise=="3":
-        answer = sum / 5.6
-        print("Anwer %.2f" % answer + "RUB")
-    elif choise==0:
-        print("bye bye!")
-    else:
-        print("Please , enter again!")
+
+def drop_less_the(nums,n):
+    return list(itertools.dropwhile(lambda x:x<n,nums))
+
+print(drop_less_the([1,2,3,4,5],3))
 
 
 
-def checkCash(s,p,m):
-    if s+p>m:
-        print("No")
-    else:
-        print("Yes")
+# Задание 4 (accumulate): Напишите функцию accumulate_sums, которая принимает
+# список целых чисел и возвращает список накопленных сумм.
+# Например, accumulate_sums([1, 2, 3, 4, 5]) должна вернуть [1, 3, 6, 10, 15].
 
-checkCash(120,100,220)
+def accumulate_sums(nums):
+    return list(itertools.accumulate(nums))
 
-def checkLucky(str):
-    n1 = int(str[0])+int(str[1])+int(str[2])
-    n2 = int(str[len(str)-1])+int(str[len(str)-2])+int(str[len(str)-3])
-    if n1 == n2 :
-        print("Lucky")
-    else:
-        print("Simple!")
-
-checkLucky("123123")
-
-n = int(input())
-partial_factorial = 1
-partial_sum = 0
-for i in range(1, n + 1):
-    partial_factorial *= i
-    partial_sum += partial_factorial
-print(partial_sum)
+print(accumulate_sums([1,2,3,4,5]))
 
 
-num=int(input("Enter num :"))
-s=""
-for i in range(1 ,num+1):
-    s+=str(i)
-    print(s)
+# Задание 3 (starmap): Напишите функцию multiply_elements, которая принимает
+# список кортежей, где каждый кортеж содержит два целых числа, и возвращает список
+# произведений чисел в каждом кортеже. Например, multiply_elements([(1, 2), (3, 4), (5,
+# 6)]) должна вернуть [2, 12, 30].
+
+def multiply_elements(multy_cast):
+    return list(itertools.starmap(lambda x,y:x*y ,multy_cast))
+
+print(multiply_elements([(1,3),(3,4)]))
+
+# Задание 2 (count): Напишите функцию count_from, которая принимает целое
+# число start и целое число n в качестве входных данных и возвращает список из n целых
+# чисел, начиная от start. Например, count_from(5, 3) должна вернуть [5, 6, 7].
+
+def count_from(start,n):
+    return list(itertools.islice(itertools.count(start),n))
+
+count_arr=count_from(1,10)
+print(count_arr)
 
 
-n = int(input())
-sum = 0
-for i in range(1, n + 1):
-    sum += i
-for i in range(n - 1):
-    sum -= int(input())
-print(sum)
+# Задание 1 (cycle): Напишите функцию cycle_string, которая принимает строку и целое
+# число n в качестве входных данных и возвращает строку, являющуюся результатом
+# повторения входной строки n раз. Например, cycle_string("abc", 3) должна
+# вернуть "abcabcabc".
+
+def cycle_string(text,n):
+    new_string=''.join(itertools.islice(itertools.cycle(text),len(text)*n))
+    return new_string
 
 
-num=int(input("Etner num :"))
-
-for i in range(1,num+1):
-    if i**2<=num:
-        print(i**2,end=" ")
-    else:
-        break
+res=cycle_string("abc",4)
+print(res)
