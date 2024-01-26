56b8bf22cf10d14ff6f9f66146f56949a889edad
richardparkerp <isabayev.b.03@gmail.com>
2024-01-27 00:09:59 +0600

    start

--- main.py
+++ main.py
@@ -1,58 +1,61 @@
+# Отрефакторить программу с прошлой домашней работы
+# ИЛИ калькулятор с практической работы на ООП стиль.
+
 import tkinter as tk
-import requests
-
-# Не понял как делать поиск по id поэтому решил сделать по параметру posts
 
 
 
 
 
-window = tk.Tk()
-window.minsize(500,500)
-entr = tk.Entry(
-    window,
-    fg="black",
-    bg="white",
-    font=("Arial Bold", 18),
-    width=25)
+class Calculator(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.operation=""
+        for i in range(1,4):
+            for j in range(3):
+                value=(i-1)*3+j+1
+                self.button=tk.Button(self,text=f"{value}",width=5,height=5,background="gray",fg="red",font=("Arial Bold",20),command=lambda x=value:self.press(x))
+                self.button.grid(row=i,column=j)
+        self.plus_button = tk.Button(self, text="+", width=5, height=3, background="gray", foreground="red",
+                                font=("Arial Bold", 20), command=lambda x="+": self.press(x))
+        self.plus_button.grid(row=1, column=3)
+        self.mult_button = tk.Button(self, text="*", width=5, height=3, background="gray", foreground="red",
+                                font=("Arial Bold", 20), command=lambda x="*": self.press(x))
+        self.mult_button.grid(row=2, column=3)
+        self.min_button = tk.Button(self, text="-", width=5, height=3, background="gray", foreground="red",
+                               font=("Arial Bold", 20), command=lambda x="-": self.press(x))
+        self.min_button.grid(row=3, column=3)
+        self.div_button = tk.Button(self, text="/", width=5, height=3, background="gray", foreground="red",
+                               font=("Arial Bold", 20), command=lambda x="/": self.press(x))
+        self.div_button.grid(row=4, column=3)
+        self.eq_button = tk.Button(self, text="=", width=5, height=3, background="gray", foreground="red",
+                              font=("Arial Bold", 20), command=lambda x=self.operation: self.eq(x))
+        self.eq_button.grid(row=0, column=3)
+        self.zero_button = tk.Button(self, text="0", width=5, height=3, background="gray", foreground="red",
+                                font=("Arial Bold", 20), command=lambda x="0": self.press(x))
+        self.zero_button.grid(row=4, column=0, columnspan=3, sticky="EW")
 
-label = tk.Label(
-    window,
-    fg="black",
-    bg="white",
-    font=("Arial Bold",20),
-    width=40,
-    height=20)
-def get_info_from_entry():
-    global entr
-    global label
-    if entr.get()!="":
-        try:
-            post_num=int(entr.get())
-            answer=requests.get(f"https://jsonplaceholder.typicode.com/posts/",params=str(post_num))
-            print(answer.json())
-            label["text"]=answer.text
-        except:
-            print(entr.get())
-            print("Error!")
+        self.label = tk.Label(
+            self,
+            text="",
+            fg="black",
+            bg="white",
+            font=("Arial Bold", 10),
+            width=5,
+            height=5)
+        self.label.grid(row=0, column=0, columnspan=3, sticky="EW")
+
+    def press(self,num):
+        self.operation+=str(num)
+        self.label["text"]=self.operation
+        print(num)
+
+    def eq(self,val):
+        answer=eval(val)
+        self.label["text"]+="="
+        self.label["text"]+=str(answer)
 
 
 
-
-label_info=tk.Label(window,text="Enter index:",fg="black",font=("Arial Bold",18))
-label_info.grid(row=0,column=0)
-entr.grid(row=1,column=0,columnspan=3,sticky="EW")
-button_search=tk.Button(
-    window,
-    text="OK",
-    fg="white",
-    background="green",
-    font=("Arial Bold",18),
-    width=5,
-    height=1,
-    command=get_info_from_entry)
-button_search.grid(row=1,column=5)
-label.grid(row=3,column=0)
-
-
-window.mainloop()
+calc=Calculator()
+calc.mainloop()
